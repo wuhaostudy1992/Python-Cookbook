56788be0bdc9863c19e03f1ba2e8b82018085685
wuhaostudy1992 <wuhaostudy1992@gmail.com>
2018-02-17 16:31:14 -0800

    update

--- c4/16.py
+++ c4/16.py
@@ -4,3 +4,10 @@ def read(s):
     for chunk in iter(lambda: s.recv(CHUNKSIZE), b''):
         pass
         # process chunk
+
+def openfile():
+    with open('test.txt') as f:
+        for chunk in iter(lambda: f.read(100), ''):
+            print(chunk)
+            
+openfile()
